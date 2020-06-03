f8c6253ae1c057f8c94c08429d5ead6f45153c2f
Shivam <53916068+ubermachine@users.noreply.github.com>
2020-06-03 23:20:32 +0530

    brrrrr

--- Easy/Reverse Linked List.js
+++ Easy/Reverse Linked List.js
@@ -1,7 +1,17 @@
+// var reverseList = function(head) {
+//     let [prev, current] = [null, head]
+//     while(current) {
+//         [current.next, prev, current] = [prev, current, current.next]
+//     }
+//     return prev
+// }
 var reverseList = function(head) {
-    let [prev, current] = [null, head]
-    while(current) {
-        [current.next, prev, current] = [prev, current, current.next]
+    let [prev, current,next] = [null, null,head]
+    while(next) {
+        current=next
+        next=current.next
+        current.next=prev
+        prev=current
     }
     return prev
 }
